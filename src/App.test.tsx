668d69363268439d9bf1ef72e7953497c7d62b54
saijo.shota.biz <saijo.shota.biz@gmail.com>
2023-03-25 13:37:06 +0900

    setting eslint

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,9 @@
+import { render } from '@testing-library/react';
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+
 import App from './App';
 
-test('', () => {
+test('sample', () => {
   render(<App />);
   // const linkElement = screen.getByText(/learn react/i);
   // expect(linkElement).toBeInTheDocument();
